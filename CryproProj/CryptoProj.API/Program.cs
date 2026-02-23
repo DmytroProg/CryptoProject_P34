79ae8b471a1be8a4187026f6571cc62667ce1971
dmytroprog <dmytroprog@gmail.com>
2026-02-23 19:15:16 +0100

    use minimal api

--- CryproProj/CryptoProj.API/Program.cs
+++ CryproProj/CryptoProj.API/Program.cs
@@ -1,7 +1,10 @@
 using System.Text;
 using CryptoProj.API;
+using CryptoProj.API.Endpoints;
 using CryptoProj.API.Middlewares;
+using CryptoProj.Storage;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
@@ -18,7 +21,7 @@ builder.Services.AddTransient<GlobalExceptionHandler>();
 
 builder.Services.AddMemoryCache();
 //builder.Services.AddHostedService<TestHostedService>();
-builder.Services.AddHostedService<CryptoAnalysisHostedService>();
+//builder.Services.AddHostedService<CryptoAnalysisHostedService>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -34,7 +37,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]!))
         };
     });
-builder.Services.AddAuthorization();
+
+builder.Services.AddAuthorization(opt => opt
+    .AddPolicy("Admin", 
+        policy => policy
+            .RequireClaim("role", "Admin")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -60,5 +67,37 @@ app.UseAuthentication(); // хто ти
 app.UseAuthorization();  // що ти можеш
 
 app.MapControllers();
+
+
+
+/*app.MapGet("api/v1/news", ()
+    =>
+{
+    Console.WriteLine("Hello World!");
+    return "News";
+});
+
+app.MapPost("api/v1/news/{id}", async ([FromBody] News news, [FromRoute] int id) =>
+{
+    return Results.Ok(news);
+});
+
+app.MapPut("api/v1/news", (HttpContext ctx, CryptoContext context, News news) =>
+    {
+        context.Update(news);
+        return Results.Ok(news);
+    })
+    .RequireAuthorization()
+    .Produces(200)
+    .WithSummary("news update endpoint");*/
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine(context.Request.Path);
+    await next(context);
+    Console.WriteLine(context.Response.StatusCode);
+});
+
+app.MapNewsEndpoints();
 
 app.Run();
